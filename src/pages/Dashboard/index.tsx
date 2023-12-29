0d1cbaaf00f693a2b45ebc17c7ca30a081db8978
CaioMar7 <caiof.marques@yahoo.com.br>
2023-12-28 22:35:47 -0300

    Paginas criadas.

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,11 @@
+import { Header } from "../../components/Header";
+
 export function Dashboard() {
     return (
         <>
-            <h1> Dashboard </h1>
-            <a href="/"> Home </a>
+            <body>
+                <Header/>
+            </body>
         </>
     )
 };
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,21 @@
+import { Header } from "../../components/Header";
+import HeroImg from "../../assets/businessman.png"
+
 export function Home() {
     return (
         <>
-            <h1> Home </h1>
-            <a href="/dashboard"> Dashboard </a>
+            <body className="w-full h-full">
+                <Header/>
+                <section className="flex items-center justify-center gap-16 border-2 border-red-500">
+                    <div>
+                        <h1 className="text-4xl text-center"> TEXTO DE EXEMPLO </h1>
+                        <p className="text-center"> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione, provident. Architecto assumenda mollitia itaque ratione enim aperiam? Ullam nisi autem ab nulla ad qui. Velit officia quod dignissimos quasi tempore?</p>
+                    </div>
+                    <div>
+                        <img className="" src={HeroImg}></img>
+                    </div>
+                </section>
+            </body>
         </>
     )
 }
