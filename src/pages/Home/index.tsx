bafa920532bacc82ee9be2d3b292302677911dc2
CaioMar7 <caiof.marques@yahoo.com.br>
2023-12-29 21:13:46 -0300

    Criada estrutura para adicionar os funcionários.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,47 +2,54 @@ import { useEffect, useState } from "react"
 
 export function Home() {
 
-    interface IUser {
-        login: string,
-        password: string
+    interface IWorker {
+        name: string,
+        job: string,
+        salary: number
+        
     }
 
-    const [userList, setUserList] = useState<IUser[]>([])
+    const [workerList, setworkerList] = useState<IWorker[]>([])
 
-    const [login, setLogin] = useState<string>("")
-    const [password, setPassword] = useState<string>("")
+    const [name, setName] = useState<string>("")
+    const [job, setJob] = useState<string>("")
+    const [salary, setSalary] = useState<number>(0)
 
     const handleSubmit = (event: React.FormEvent) => {
         event.preventDefault()
         
-        const newUser: IUser = {login, password}
-        setUserList(prevUserList => [... prevUserList, newUser])
+        const newWorker: IWorker = {name, job, salary}
+        setworkerList(prevworkerList => [... prevworkerList, newWorker])
     }
 
     useEffect( () => {
-        const localUserList = localStorage.getItem('@userList')
-        if(localUserList) {
-            setUserList(JSON.parse(localUserList))
+        const localworkerList = localStorage.getItem('@workerList')
+        if(localworkerList) {
+            setworkerList(JSON.parse(localworkerList))
         }
     }, [])
 
     useEffect( () => {
-        if (userList.length > 0) {
-            localStorage.setItem('@userList', JSON.stringify(userList))
+        if (workerList.length > 0) {
+            localStorage.setItem('@workerList', JSON.stringify(workerList))
         } 
-    }, [userList])
+    }, [workerList])
 
     return (
         <>
             <div className="h-screen flex items-center justify-center">
                 <form className="flex flex-col gap-4 items-center justify-center border-2 border-gray-500 shadow h-60 w-96" onSubmit={(event) => handleSubmit(event)}>
                     <div className="flex justify-between items-center w-full px-10">
-                            <label className="flex justify-between items-center font-bold"> Login </label>
-                            <input className="mx-4 border-2 border-gray-300 px-1" type="text" name="login" onChange={(event) => setLogin(event.target.value)}/>
+                            <label className="flex justify-between items-center font-bold"> Nome </label>
+                            <input className="mx-4 border-2 border-gray-300 px-1" type="text" name="name" onChange={(event) => setName(event.target.value)}/>
                         </div>
                         <div className="flex justify-between items-center w-full px-10">
-                            <label className="flex justify-between items-center font-bold"> Password </label>
-                            <input className="mx-4 border-2 border-gray-300 px-1" type="password" name="login" onChange={(event) => setPassword(event.target.value)}/>
+                            <label className="flex justify-between items-center font-bold"> Função </label>
+                            <input className="mx-4 border-2 border-gray-300 px-1" type="text" name="job" onChange={(event) => setJob(event.target.value)}/>
+                        </div>
+                        <div className="flex justify-between items-center w-full px-10">
+                            <label className="flex justify-between items-center font-bold"> Salário </label>
+                            <input className="mx-4 border-2 border-gray-300 px-1" type="number" name="salary" onChange={(event) => setSalary(Number(event.target.value))}/>
                         </div>
                         <div className="flex gap-6">
                             <button className="mt-6 border-2 border-gray-500 h-12 w-24 font-bold" onClick={(event) => handleSubmit(event)}> Entrar </button>
@@ -52,8 +59,8 @@ export function Home() {
             </div>
             <div>
                {
-                userList && userList.map( (user:IUser, index:number) => (
-                    <li key={index}> {`Login: ${user.login} / Password: ${user.password}`} </li>
+                workerList && workerList.map( (worker:IWorker, index:number) => (
+                    <li key={index}> {`Nome: ${(worker.name).toUpperCase()} / Função: ${(worker.job).toUpperCase()} / Salário: R$ ${worker.salary}`} </li>
                 ))
                }
             </div>
