e32c54d611612608115532ed7aa3aaa45451b82b
CaioMar7 <caiof.marques@yahoo.com.br>
2023-12-30 23:16:08 -0300

    Adicionado novas features de calculos.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,7 +7,7 @@ export function Home() {
         name: string,
         job: string,
         section: string,
-        salary: number
+        salary: number | undefined
     }
 
     const [workerList, setworkerList] = useState<IWorker[]>([])
@@ -15,7 +15,7 @@ export function Home() {
     const [name, setName] = useState<string>("")
     const [job, setJob] = useState<string>("")
     const [section, setSection] = useState<string>("")
-    const [salary, setSalary] = useState<number>(0)
+    const [salary, setSalary] = useState<number>()
 
     const handleSubmit = (event: React.FormEvent) => {
         event.preventDefault()
@@ -38,6 +38,25 @@ export function Home() {
         localStorage.setItem('@workerList', JSON.stringify(newWorkerList))
     }
 
+    const calcAllSalary = () : number => {
+        return workerList.reduce((acc, worker) => acc + (worker.salary || 0), 0);
+    }
+    
+    const calcSalaryBySection = (section:string) : number => {
+        const filteredWorkerList = workerList.filter((worker) => worker.section === section && worker.salary !== undefined);
+        return filteredWorkerList.reduce((acc, worker) => acc + (worker.salary || 0), 0);
+    }
+
+    const getAllSections = () : string[] => {
+        const workerSections : string[] = []
+        workerList.map( (worker) => {
+            if(!workerSections.includes(worker.section)) {
+                workerSections.push(worker.section)
+            }
+        })
+        return workerSections
+    }
+
     useEffect( () => {
         const localworkerList = localStorage.getItem('@workerList')
         if(localworkerList) {
@@ -57,15 +76,15 @@ export function Home() {
                 <form className="flex flex-col gap-4 items-center justify-center border-2 border-gray-500 shadow h-full w-96 py-8" onSubmit={(event) => handleSubmit(event)}>
                     <div className="flex justify-between items-center w-full px-10">
                             <label className="flex justify-between items-center font-bold"> Nome </label>
-                            <input className="mx-4 border-2 border-gray-300 px-1" type="text" value={name} name="name" onChange={(event) => setName(event.target.value)}/>
+                            <input className="mx-4 border-2 border-gray-300 px-1" type="text" value={name} name="name" onChange={(event) => setName((event.target.value).toUpperCase())}/>
                         </div>
                         <div className="flex justify-between items-center w-full px-10">
                             <label className="flex justify-between items-center font-bold"> Função </label>
-                            <input className="mx-4 border-2 border-gray-300 px-1" type="text" value={job} name="job" onChange={(event) => setJob(event.target.value)}/>
+                            <input className="mx-4 border-2 border-gray-300 px-1" type="text" value={job} name="job" onChange={(event) => setJob((event.target.value).toUpperCase())}/>
                         </div>
                         <div className="flex justify-between items-center w-full px-10">
                             <label className="flex justify-between items-center font-bold"> Setor </label>
-                            <input className="mx-4 border-2 border-gray-300 px-1" type="text" value={section} name="setor" onChange={(event) => setSection(event.target.value)}/>
+                            <input className="mx-4 border-2 border-gray-300 px-1" type="text" value={section} name="setor" onChange={(event) => setSection((event.target.value).toUpperCase())}/>
                         </div>
                         <div className="flex justify-between items-center w-full px-10">
                             <label className="flex justify-between items-center font-bold"> Salário </label>
@@ -78,9 +97,26 @@ export function Home() {
                 </form>
             </div>
             <div>
+                {
+                    workerList.length > 0 && (
+                        <h1> Total de salários: {calcAllSalary()} </h1>
+                    )
+                }
+
+                {
+                    getAllSections().length > 0 &&  (   
+                        <h1> Total de salários por setor: </h1>
+                    )
+                }
+                {
+                    getAllSections().map( (section, index:number) => (
+                        <li key={index}>{`Setor: ${section} - Total: ${calcSalaryBySection(section)}`}</li>
+                    ))
+                }
+
                {
                 workerList && workerList.map( (worker:IWorker, index:number) => (
-                    <li key={index}> {`ID: ${worker.id} - Nome: ${(worker.name).toUpperCase()} / Função: ${(worker.job).toUpperCase()} / Setor: ${section} / Salário: R$ ${worker.salary}`} <button onClick={() => removeWorker(worker.id)}> X <button/> </button></li>
+                    <li key={index}> {`ID: ${worker.id} - Nome: ${(worker.name).toUpperCase()} / Função: ${(worker.job).toUpperCase()} / Setor: ${(worker.section).toUpperCase()} / Salário: R$ ${worker.salary}`} <button onClick={() => removeWorker(worker.id)}> X <button/> </button></li>
                 ))
                }
             </div>
